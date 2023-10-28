2a7b00b408186f5512d8389d1a827e1b292a5bdb
timoumi12 <ihebtimoumi1@gmail.com>
2023-10-28 22:20:53 +0100

    fixing task 2

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,16 @@
 """index.py file"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 @app_views.route('/status')
 def index():
     """returns OK"""
     status = {"status": "OK"}
     return jsonify(status)
+
+@app_views.route('/stats')
+def _count():
+    stats = {
+        "amenities": storage.count()
+    }
