eb46bd284e850482dfe4c901aa2a9c11ec002df5
timoumi12 <ihebtimoumi1@gmail.com>
2023-10-29 11:11:03 +0100

    count stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,12 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from models.review import Review
+from models.user import User
+from models.city import City
+from models.state import State
+from models.place import Place
 
 
 @app_views.route('/status')
@@ -14,6 +20,10 @@ def index():
 
 @app_views.route('/stats')
 def _count():
-    stats = {
-        "amenities": storage.count()
-    }
+    stats = {"Amenity": storage.count(Amenity),
+               "City": storage.count(City),
+               "Place": storage.count(Place),
+               "Review": storage.count(Review),
+               "State": storage.count(State),
+               "User": storage.count(User)}
+    return jsonify(stats)
