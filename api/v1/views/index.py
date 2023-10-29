63527be60696c9f7558eec97a16bdf5a2ca1a541
timoumi12 <ihebtimoumi1@gmail.com>
2023-10-29 10:39:31 +0100

    fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,11 +4,13 @@ from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
+
 @app_views.route('/status')
 def index():
     """returns OK"""
     status = {"status": "OK"}
     return jsonify(status)
+
 
 @app_views.route('/stats')
 def _count():
